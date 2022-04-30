e9542e9e8e5f8348ebc29f281d9b6acb59dc4f58
Ahnaf Mubashshir Mobin <99656046+AhnafMubashshir@users.noreply.github.com>
2022-04-30 22:41:36 +0600

    Gauss-Seidel method
    Tried to add a new feature...equation solving...But cant get lower error...challenges are to minimize this error.

--- Test.cpp
+++ Test.cpp
@@ -1,4 +1,5 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
 // `M × N` matrix
 #define M 4
@@ -7,36 +8,47 @@
 // Dynamic Memory Allocation in C++ for 2D Array
 int main()
 {
-    // dynamically create an array of pointers of size `M`
-    int** A = new int*[M];
+    int n;
 
-    // dynamically allocate memory of size `N` for each row
-    for (int i = 0; i < M; i++) {
-        A[i] = new int[N];
-    }
+//    Number of equation
+    cin>>n;
 
-    // assign values to the allocated memory
-    for (int i = 0; i < M; i++)
-    {
-        for (int j = 0; j < N; j++) {
-            A[i][j] = rand() % 100;
+    double eqn[n+1][n+1], eqn_equal[n+1];
+
+    for(int i=1; i<n+1; i++){
+        for(int j=1; j<n+1; j++){
+            cin>>eqn[i][j];
         }
+        cin>>eqn_equal[i];
     }
 
-    // print the 2D array
-    for (int i = 0; i < M; i++)
-    {
-        for (int j = 0; j < N; j++) {
-            std::cout << A[i][j] << " ";
+    double new_val[n+1]={}, old_val[n+1]={}, error[n+1];
+
+    while(1){
+        for(int i=1; i<n+1; i++){
+            old_val[i]= new_val[i];
         }
-        std::cout << std::endl;
+
+        for(int i=1; i<n+1; i++){
+            for(int j=1; j<n+1; j++){
+                if(j!=i) new_val[i]= eqn_equal[i]-(eqn[i][j]*new_val[j]);
+            }
+            new_val[i]/=eqn[i][i];
+        }
+        for(int i=1; i<n+1; i++) cout<<new_val[i]<<" ";
+        cout<<endl;
+
+        for(int i=1; i<n+1; i++){
+            error[i]= abs(new_val[i]-old_val[i])/new_val[i];
+            error[i]*=100;
+        }
+
+        if(*max_element(error, error+n+1)<=1) break;
     }
 
-    // deallocate memory using the delete operator
-    for (int i = 0; i < M; i++) {
-        delete[] A[i];
+    for(int i=1; i<n+1; i++){
+        cout<<new_val[i]<<" ";
     }
-    delete[] A;
 
     return 0;
 }
