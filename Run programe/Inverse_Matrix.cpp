4ff60c96ba19a209ea1a49b93d4f6863b329c18d
Ahnaf Mubashshir Mobin <99656046+AhnafMubashshir@users.noreply.github.com>
2022-05-23 22:49:11 +0600

    Some changes

--- Run programe/Inverse_Matrix.cpp
+++ Run programe/Inverse_Matrix.cpp
@@ -1,40 +1,75 @@
 #include "header.h"
 
-void Inverse_Matrix()
+//Inverse matrix
+double inverse_matrix(double **mat, double **inv_mat, int r, int c)
 {
-    cout<<"#Inverse Matrix"<<endl<<endl;
-    int row, col;
+    int i,j, sign=1;
+    double det;
 
-    cout<<"Enter row & column of the matrix: ";
-    cin>>row>>col;
-    cout<<endl;
+    double** sub_mat= new double*[r];
+    for(int i=0; i<r; i++) sub_mat[i]= new double[c];
 
+    double** Adj_mat= new double*[r];
+    for(int i=0; i<r; i++) Adj_mat[i]= new double[c];
 
-    //Taking 1st matrix input
-    double** matrix=new double*[row];
-    for(int i=0; i<row; i++) matrix[i]= new double[col];
+    double** t_Adj_mat= new double*[c];
+    for(int i=0; i<c; i++) t_Adj_mat[i]= new double[r];
 
-    cout<<"Enter The matrix: "<<endl;
-    for(int i=0; i<row; i++){
-        for(int j=0; j<col; j++){
-            cin>>matrix[i][j];
+    double** temp= new double*[c];
+    for(int i=0; i<c; i++) temp[i]= new double[r];
+
+    double** temp1= new double*[c];
+    for(int i=0; i<c; i++) temp1[i]= new double[c];
+
+    double** temp2= new double*[c];
+    for(int i=0; i<c; i++) temp2[i]= new double[r];
+
+    //using pseudo inverse
+    transpose_matrix(mat, temp, r, c); //temp is the conjugated transpose matrix
+    multiply_matrix(temp, mat, temp1, c, r, c);
+
+    det=determenent(temp1, c, c);
+
+    if(det==0) return det;
+
+    //adjacence matrix
+    if(c==2){
+        t_Adj_mat[0][0]=temp1[1][1];
+        t_Adj_mat[1][1]=temp1[0][0];
+        t_Adj_mat[0][1]=-temp1[0][1];
+        t_Adj_mat[1][0]=-temp1[1][0];
+    }
+    else{
+        for(i=0;i<c;i++){
+            for(j=0;j<c;j++){
+                sub_matrix_initializer(temp1, sub_mat, c, c, i, j);
+                Adj_mat[i][j]=(sign*determenent(sub_mat, c-1, c-1));
+                sign=-sign;
+            }
+        }
+        transpose_matrix(Adj_mat, t_Adj_mat, c, c);
+    }
+
+    for(i=0;i<c;i++){
+        for(j=0;j<c;j++){
+            t_Adj_mat[i][j]/=det;
         }
     }
-    cout<<endl;
 
-    double** result=new double*[col];
-    for(int i=0; i<col; i++) result[i]= new double[row];
+    multiply_matrix(t_Adj_mat, temp, temp2, c, c, r);
 
-    double det= inverse_matrix(matrix, result, row, col);
-    if(det==0){
-        cout<<"::Could not find Inverse. (determinant is 0)."<<endl<<endl;
-        return;
+    //inversed matrix
+    for(i=0;i<c;i++){
+        for(j=0;j<r;j++){
+            inv_mat[i][j]=temp2[i][j];
+        }
     }
 
-    cout<<"The Inverse Matrix is: "<<endl;
-
-    print_matrix(result, col, row);
-
-    delete[] matrix;
-    delete[] result;
+    delete[] sub_mat;
+    delete[] Adj_mat;
+    delete[] t_Adj_mat;
+    delete[] temp;
+    delete[] temp1;
+    delete[] temp2;
+return 1;
 }
--- Run programe/Transpose_Matrix.cpp
+++ Run programe/Transpose_Matrix.cpp
@@ -1,36 +1,13 @@
+#include<bits/stdc++.h>
 #include "header.h"
+using namespace std;
 
-void Transpose_Matrix()
+//Transpose Matrix
+void transpose_matrix(double **mat, double **transpose_mat, int r, int c)
 {
-    cout<<"#Transpose Matrix"<<endl<<endl;
-    int row, col;
-
-    cout<<"Enter row & column of the matrix: ";
-    cin>>row>>col;
-    cout<<endl;
-
-
-    //Taking 1st matrix input
-    double** matrix=new double*[row];
-    for(int i=0; i<row; i++) matrix[i]= new double[col];
-
-    cout<<"Enter The matrix: "<<endl;
-    for(int i=0; i<row; i++){
-        for(int j=0; j<col; j++){
-            cin>>matrix[i][j];
+    for(int i=0;i<r; i++){
+        for(int j=0;j<c;j++){
+            transpose_mat[j][i]=mat[i][j];
         }
     }
-    cout<<endl;
-
-
-    double** T_matrix=new double*[col];
-    for(int i=0; i<col; i++) T_matrix[i]= new double[row];
-
-    transpose_matrix(matrix, T_matrix, row, col);
-
-    cout<<"The Transposed matrix is: "<<endl;
-    print_matrix(T_matrix, col, row);
-
-    delete[] matrix;
-    delete[] T_matrix;
 }
